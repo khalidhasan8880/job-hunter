c7172f74d91cfeaee9f508e94a65f232de6aff15
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-04-10 15:10:26 +0600

    added job category section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from '../Banner/Banner';
-import Header from '../Header/Header';
+import JobCategorysCard from '../JobCategorysCard/JobCategorysCard';
+import './Home.css'
 const Home = () => {
+    const [data, setData]= useState([])
+
+    useEffect(()=>{
+        fetch('job_category.json')
+        .then(res=>res.json())
+        .then(data=> setData(data))
+    },[])
+
     return (
         <div>
-            <div className='bg-sky-100'>
+            <div className='bg-primary'>
                 <Banner></Banner>
+                
             </div>
 
 
-            <div className='mt-36'>
-                <h2 className='md:text-4xl text-3xl font-bold  text-center my-5font-bold'>Job Category List</h2>
-                <p className='description text-center my-5'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className='mt-36 flex flex-col justify-center items-center'>
+                <h2 className='md:text-4xl text-3xl font-bold  my-5 '>Job Category List</h2>
+                <p className='description my-5'>Explore thousands of job opportunities with all the information you need. Its your future</p>
                 
-                <div>
-
+                <div className=' flex flex-wrap gap-6 px-44 pt-9 mb-10'>
+                    {
+                        data.map(category=> <JobCategorysCard category={category}></JobCategorysCard> )
+                    }
                 </div>
             </div>
+
+            
         </div>
     );
 };
--- src/components/JobCategorysCard/JobCategorysCard.jsx
+++ src/components/JobCategorysCard/JobCategorysCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const JobCategorysCard = () => {
+const JobCategorysCard = ({category}) => {
+    const {category_name, picture,available_job } = category;
     return (
-        <div>
-            
+        <div className='w-72 bg-primary rounded-md p-9'>
+            <img className='w-11 h-11 my-4' src={picture} alt="" />
+            <h3 className='my-4 text-2xl font-semibold'>{category_name}</h3>
+            <p className='description'>{available_job} Jobs Available</p>
         </div>
     );
 };
